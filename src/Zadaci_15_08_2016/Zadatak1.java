cf5e086b5c76c8fd02d9c861a4e1f5d422ff7667
Hasan Halilovic <hasan4halilovic@hotmail.com>
2016-08-15 20:43:55 +0200

    Update Zadatak1.java

--- src/Zadaci_15_08_2016/Zadatak1.java
+++ src/Zadaci_15_08_2016/Zadatak1.java
@@ -3,7 +3,10 @@ package Zadaci_15_08_2016;
 public class Zadatak1 {
 
 	public static void main(String[] args) {
+		//Pravimo nasumicno biranje brojeva od 1 do 12 
 		int num = (int) (Math.random() * 12 + 1);
+		//Ako je broj 1 ispisuje da je to mjesec januar
+		//Tako radi za svih 12 mjeseci koji broj izabere ispise ime mjeseca za koji je broj izabrao tj za svih 12 mjeseci 
 		if (num == 1) {
 			System.out.println("Januar");
 		} else if (num == 2) {
